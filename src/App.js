a855613858614f4e5eaf67993a999583b68fb839
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-13 02:49:37 -0400

    Nav component functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
 import React from 'react'
 import './App.css';
-import About from './about'
+import Nav from './Nav'
+import About from './About'
 
 function App() {
   return (
-    <About></About>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+    
   );
 }
 
