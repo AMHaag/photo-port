4b2b019a5735450e8e989c68f621f0c72413ce06
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-13 10:12:04 -0400

    added About tests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-// import logo from './logo.svg';
-import React from 'react'
-import './App.css';
-import Nav from './Nav'
-import About from './About'
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
     </div>
-    
   );
 }
 
